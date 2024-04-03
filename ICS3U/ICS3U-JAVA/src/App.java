b329e41d34769eb0036df3dc83ab04eabcd86d81
Yiling <yiling.ye@student.tdsb.on.ca>
2024-04-03 13:55:40 -0400

    java

---
+++ ICS3U/ICS3U-JAVA/src/App.java
@@ -0,0 +1,9 @@
+//This page is created by Yiling Ye
+//On "Thurs, Marth 21st 2024"
+//To "create for a class for our first Java App"
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
